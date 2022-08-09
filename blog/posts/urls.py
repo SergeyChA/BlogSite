535f8c5924fae87e24f5880c7964926a999970e4
SergeyChA <satilit2019@gmail.com>
2022-08-09 10:43:56 +0300

    add CRUD posts

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
-from .views import PostsListView, PostsDetailView, PostsCreateView,contacts
+from .views import (PostsListView, 
+                    PostsDetailView, 
+                    PostsCreateView, 
+                    PostsUpdateView, 
+                    PostsDeleteView, 
+                    contacts)
 
 urlpatterns = [
     path('', PostsListView.as_view(), name='home'),
     path('contacts/', contacts, name='contacts'),
     path('post/<int:pk>', PostsDetailView.as_view(), name='post'),
     path('post/create', PostsCreateView.as_view(), name='post_create'),
+    path('post/update/<int:pk>', PostsUpdateView.as_view(), name='post_update'),
+    path('post/delete/<int:pk>', PostsDeleteView.as_view(), name='post_delete'),
+
 
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,27 +1,68 @@
 from django.shortcuts import render
 from .models import Posts
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .form import PostsCreateForm
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 class PostsListView(ListView):
     model = Posts
     ordering = ['-pub_date']
 
+
 class PostsDetailView(DetailView):
     model = Posts
-    
-class PostsCreateView(CreateView):
+
+   
+class PostsCreateView(LoginRequiredMixin, CreateView):
     model = Posts
     form_class = PostsCreateForm
 
+    def get_context_data(self, **kwargs):
+        context = super(PostsCreateView, self).get_context_data(**kwargs)
+        context['title'] = 'Написать статью'
+        context['button_submit'] = 'Опубликовать'
+        return context
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         print(self.request.user)
         return super().form_valid(form)
 
+
+class PostsUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Posts
+    form_class = PostsCreateForm
+    template_name = 'posts/posts_form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostsUpdateView, self).get_context_data(**kwargs)
+        context['title'] = 'Редактирование статьи'
+        context['button_submit'] = 'Редактировать'
+        return context
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        print(self.request.user)
+        return super().form_valid(form)
+
+
+class PostsDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Posts
+    success_url = 'home'
+    template_name = 'posts/posts_delete.html'
     
-    
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def contacts(request):
